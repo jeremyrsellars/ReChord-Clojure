87ba8fbe6528280efc3be3f34eeb5d8c82c6728d
Jeremy Sellars <jeremyrsellars@gmail.com>
2013-10-27 18:42:50 -0500

    get-line-type and tests

---
+++ src/rechord/linereader.clj
@@ -0,0 +1,8 @@
+(ns rechord.linereader)
+
+(defn get-line-type [line]
+   (condp re-matches line
+     #"^((\s*[A-G][b#]?[Mm]?(aj|sus)?\d*)/?)+\W*$" :chord
+     #"^\s*$" :separator
+     :lyric))
+
---
+++ test/rechord/linereader_test.clj
@@ -0,0 +1,26 @@
+(ns rechord.linereader-test
+  (:require [clojure.test :refer :all]
+            [rechord.linereader :refer :all]))
+
+;; get-line-type
+(deftest empty-is-separator
+  (testing "empty lines are separators"
+    (is (= :separator
+      (get-line-type "")))))
+
+(deftest A-is-chords
+  (testing "A single 'A' is a chord"
+    (is (= :chord
+      (get-line-type "A")))))
+
+(deftest A-B-C-is-chords
+  (testing "chord line"
+    (is (= :chord
+      (get-line-type " A B       C ")))))
+
+(deftest A-birthday-song-is-lyrics
+  (testing "note-offsets are based on A=0, A#=1, etc."
+    (is (= :lyric
+      (get-line-type "A birthday song")))))
+
+
