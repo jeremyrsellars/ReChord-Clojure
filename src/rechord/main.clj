981a87bb2518d17eeeac885d99da09807afa23b6
Jeremy Sellars <jeremyrsellars@gmail.com>
2013-10-28 21:49:57 -0500

    rechord-line and tests

---
+++ src/rechord/main.clj
@@ -0,0 +1,12 @@
+(ns rechord.main
+  (:require [rechord.core :refer :all]))
+
+(defn rechord-line [tagged-line offset note-selector]
+  (let [tag (first tagged-line)
+        line (second tagged-line)]
+    (condp = tag
+      :separator line
+      :lyric     line
+      :chord     (replace-chords line offset note-selector)
+      line)))
+
---
+++ test/rechord/main_test.clj
@@ -0,0 +1,27 @@
+(ns rechord.main-test
+  (:require [clojure.test :refer :all]
+            [rechord.core :refer :all]
+            [rechord.main :refer :all]))
+
+;; rechord-line
+(deftest rechord-line-separator-is-identity
+  (testing "rechord-line [:separator :x] is :x"
+    (is (= :x
+           (rechord-line [:separator :x] -1 prefer-flats)))))
+
+(deftest rechord-line-lyric-is-identity
+  (testing "rechord-line [:lyric :y] is :y"
+    (is (= :y
+           (rechord-line [:lyric :y] -1 prefer-flats)))))
+
+(deftest rechord-line-other-is-identity
+  (testing "rechord-line [:other :x] is :x"
+    (is (= :x
+           (rechord-line [:other :x] -1 prefer-flats)))))
+
+(deftest rechord-line-chord-is-rechorded
+  (testing "rechord-line [:chord 'A B C'] is :y"
+    (is (= "Ab Bb B"
+           (rechord-line [:chord "A B C"] -1 prefer-flats)))))
+
+
