fbabb3c0ff88c446e79c9baa7515108bf03b166d
Jeremy Sellars <jeremyrsellars@gmail.com>
2013-10-31 22:29:39 -0500

    expanded node module

--- src/node.cljs
+++ src/node.cljs
@@ -3,11 +3,17 @@
             [rechord.core :as core]
             [rechord.linereader :as linereader]
             [rechord.main :as main]))
- 
+
 (defn -main [& mess]
-  (set! module.exports.main main/rechord)
+  (set! module.exports.rechord main/rechord)
+  (set! module.exports.rechord-tagged-lines main/rechord-tagged-lines)
+
+  (set! module.exports.getTaggedLines (fn [text] (to-array (map to-array (linereader/get-tagged-lines text)))))
+  (set! module.exports.getLineType linereader/get-line-type)
+
   (set! module.exports.preferSharps core/prefer-sharps)
   (set! module.exports.preferFlats core/prefer-flats)
   )
- 
+
 (set! *main-cli-fn* -main)
+
