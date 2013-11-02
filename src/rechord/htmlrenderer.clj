e86e4f0057eb07ac98b9cdfb57cf40a80c7c5c00
Jeremy Sellars <jeremyrsellars@gmail.com>
2013-11-02 00:09:50 -0500

    render rudimentary html

---
+++ src/rechord/htmlrenderer.clj
@@ -0,0 +1,11 @@
+(ns rechord.htmlrenderer
+  (:require [clojure.string :as string]))
+
+(defn render-chord-line [text]
+  (string/replace text #" |\S+"
+     #(if (= " " %) "&nbsp;"
+        (string/join ["<span>" % "</span>"]))))
+
+(defn render-lyric-line [text]
+  (string/replace text #" " "&nbsp;"))
+
---
+++ test/rechord/htmlrenderer_test.clj
@@ -0,0 +1,28 @@
+(ns rechord.htmlrenderer-test
+  (:require [clojure.test :refer :all]
+            [rechord.core :refer :all]
+            [rechord.htmlrenderer :refer :all]))
+
+;; render-chord-line
+(deftest render-chord-line-html
+  (testing "render-chord-line [text]"
+    (is (= ""
+           (render-chord-line "")))
+    (is (= "&nbsp;"
+           (render-chord-line " ")))
+    (is (= "<span>A</span>"
+           (render-chord-line "A")))
+    ))
+
+
+;; render-lyric-line
+(deftest render-lyric-line-html
+  (testing "render-lyric-line [text]"
+    (is (= ""
+           (render-lyric-line "")))
+    (is (= "&nbsp;"
+           (render-lyric-line " ")))
+    (is (= "&nbsp;A&nbsp;"
+           (render-lyric-line " A ")))
+    ))
+
